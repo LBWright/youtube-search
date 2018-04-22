1e351ee3d8b01c6b886d6f9ebe362eeeee42ae8f
LBWright <lwright.js@gmail.com>
2018-04-22 12:14:36 -0600

    Added styling and functionality to videos. Can now click a video to display it in the embed.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyDufmS0s6odtXVpmOiXnku4kwXtzAT0IwY';
 
@@ -12,10 +13,16 @@ class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
 
         YTSearch({key: API_KEY, term:'ReactJS'}, (videos) => {
-            this.setState({ videos })
+            this.setState({ 
+                videos:videos,
+                selectedVideo: videos[0]
+            })
         });
     }
     render(){
@@ -23,7 +30,11 @@ class App extends Component {
         return( 
         <div>
             <SearchBar />
-            <VideoList videos={this.state.videos} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList 
+            onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+            videos={this.state.videos} 
+            />
         </div>
         );
     }
